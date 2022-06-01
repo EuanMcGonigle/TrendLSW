e929127314b246443a30cf8dd22c50d5406c6921
Euan McGonigle <kz20709@bristol.ac.uk>
2022-06-01 11:24:07 +0100

    update code style

--- R/simple.spec.plot.R
+++ R/simple.spec.plot.R
@@ -1,10 +1,8 @@
-simple.spec.plot = function(spectrum, bylev = FALSE){
-
-  scales = wavethresh::nlevelsWT(spectrum)
-  if (bylev ==FALSE){
+simple.spec.plot <- function(spectrum, bylev = FALSE) {
+  scales <- wavethresh::nlevelsWT(spectrum)
+  if (bylev == FALSE) {
     plot(spectrum, ylabchars = -(1:scales), main = "", sub = "", ylab = "Scale")
-  }
-  else{
+  } else {
     plot(spectrum, ylabchars = -(1:scales), main = "", sub = "", ylab = "Scale", scaling = "by.level")
   }
 }
