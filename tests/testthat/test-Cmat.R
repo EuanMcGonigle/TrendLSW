bf2c5621729a1fbdef3de0d88c748f0c33743f3e
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2023-06-16 17:32:12 +0100

    Update test-Cmat.R

--- tests/testthat/test-Cmat.R
+++ tests/testthat/test-Cmat.R
@@ -1,7 +1,7 @@
 test_that("Cmat.calc executes with larger gen wavelet", {
   skip_on_cran()
   J <- 5
-  C <- Cmat.calc(J=J, gen.filter.number = 2)
+  C <- Cmat.calc(J = J, gen.filter.number = 2)
   expect_equal(class(C), c("matrix", "array"))
   expect_equal(ncol(C), J)
   expect_equal(nrow(C), J)
@@ -10,25 +10,29 @@ test_that("Cmat.calc executes with larger gen wavelet", {
 test_that("Cmat.calc executes with larger an wavelet", {
   skip_on_cran()
   J <- 5
-  C <- Cmat.calc(J=J, an.filter.number = 2)
+  C <- Cmat.calc(J = J, an.filter.number = 2)
   expect_equal(class(C), c("matrix", "array"))
   expect_equal(ncol(C), J)
   expect_equal(nrow(C), J)
 })
 
 test_that("positive J required", {
-  expect_error(Cmat.calc(J = -5),
-               "The parameter J should be a positive integer.")
+  expect_error(
+    Cmat.calc(J = -5),
+    "The parameter J should be a positive integer."
+  )
 })
 
 test_that("numeric J required", {
-  expect_error(Cmat.calc(J = "5"),
-               "The parameter J should be a positive integer.")
+  expect_error(
+    Cmat.calc(J = "5"),
+    "The parameter J should be a positive integer."
+  )
 })
 
 test_that("integer J required", {
-  expect_error(Cmat.calc(J = "5.5"),
-               "The parameter J should be a positive integer.")
+  expect_error(
+    Cmat.calc(J = "5.5"),
+    "The parameter J should be a positive integer."
+  )
 })
-
-
