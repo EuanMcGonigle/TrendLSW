111b06b54bf7ae006883aea458a7a99732cc31c5
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2023-06-20 15:38:22 +0100

    Update test-wav.trend.R

--- tests/testthat/test-wav.trend.R
+++ tests/testthat/test-wav.trend.R
@@ -1,3 +1,48 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("wav.trend.est executes", {
+  skip_on_cran()
+  x <- stats::rnorm(256)+seq(from = 0, to = 4, length = 256)
+  x.t <- wav.trend.est(x,  boundary.handle = FALSE)
+  expect_equal(class(x.t), "list")
+})
+
+test_that("wav..trend.est executes with boundary handling", {
+  skip_on_cran()
+  x <- stats::rnorm(256)+seq(from = 0, to = 4, length = 256)
+  x.t <- wav.trend.est(x, boundary.handle = TRUE)
+  expect_equal(class(x.t), "list")
+})
+
+test_that("wav.trend.est executes with nondecimated tranform", {
+  skip_on_cran()
+  x <- stats::rnorm(256)+seq(from = 0, to = 4, length = 256)
+  x.t <- wav.trend.est(x,  type = "nondec")
+  expect_equal(class(x.t), "list")
+})
+
+test_that("wav.trend.est executes with confidence interval", {
+  skip_on_cran()
+  x <- stats::rnorm(256)+seq(from = 0, to = 4, length = 256)
+  x.t <- wav.trend.est(x,  calc.confint = TRUE)
+  expect_equal(class(x.t), "list")
+})
+
+test_that("wav.trend.est executes with confidence interval and non-dyadic data", {
+  skip_on_cran()
+  x <- stats::rnorm(230)+seq(from = 0, to = 4, length = 230)
+  x.t <- suppressWarnings(wav.trend.est(x,  calc.confint = TRUE))
+  expect_equal(class(x.t), "list")
+})
+
+test_that("wav.trend.est executes with non-dyadic data", {
+  skip_on_cran()
+  x <- stats::rnorm(230)+seq(from = 0, to = 4, length = 230)
+  x.t <- suppressWarnings(wav.trend.est(x))
+  expect_equal(class(x.t), "list")
+})
+
+test_that("wav.trend.est executes with confidence interval and boundary handling", {
+  skip_on_cran()
+  x <- stats::rnorm(256)+seq(from = 0, to = 4, length = 256)
+  x.t <- wav.trend.est(x,  calc.confint = TRUE, boundary.handle = TRUE)
+  expect_equal(class(x.t), "list")
 })
