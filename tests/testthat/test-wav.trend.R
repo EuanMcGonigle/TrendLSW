220648d90a67ef9d0467ddb9c7b1a478a56e0805
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2023-06-20 16:44:50 +0100

    Update test-wav.trend.R

--- tests/testthat/test-wav.trend.R
+++ tests/testthat/test-wav.trend.R
@@ -46,3 +46,25 @@ test_that("wav.trend.est executes with confidence interval and boundary handling
   x.t <- wav.trend.est(x,  calc.confint = TRUE, boundary.handle = TRUE)
   expect_equal(class(x.t), "list")
 })
+
+test_that("calc.confint is logical", {
+  expect_error(
+    wav.trend.est(stats::rnorm(64), calc.confint = "true"),
+    "Parameter calc.confint must be logical variable"
+  )
+})
+
+test_that("calc.confint can't be used with nondec transform", {
+  expect_error(
+    wav.trend.est(stats::rnorm(64), calc.confint = TRUE, transform.type = "nondec"),
+    "Only transform.type = 'dec' is supported for calculating confidence
+            intervals"
+  )
+})
+
+test_that("transform.type recognised", {
+  expect_error(
+    wav.trend.est(stats::rnorm(64), transform.type = "packet"),
+    "Parameter transform.type must be either 'dec' or 'nondec'"
+  )
+})
