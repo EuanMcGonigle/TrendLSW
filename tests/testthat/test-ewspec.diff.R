e3ec333f9738a5f1cb1a1bb51100573bd77c0909
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2023-06-20 14:16:49 +0100

    Update test-ewspec.diff.R

--- tests/testthat/test-ewspec.diff.R
+++ tests/testthat/test-ewspec.diff.R
@@ -5,6 +5,13 @@ test_that("ewspec.diff executes", {
   expect_equal(class(x.s), "list")
 })
 
+test_that("ewspec.diff executes with diff.number = 2", {
+  skip_on_cran()
+  x <- stats::rnorm(256)
+  x.s <- ewspec.diff(x, diff.number = 2)
+  expect_equal(class(x.s), "list")
+})
+
 test_that("ewspec.diff executes with supplied matrix", {
   skip_on_cran()
   x <- stats::rnorm(256)
@@ -40,6 +47,24 @@ test_that("ewspec.diff warns for boundary handling on non-dyadic data", {
   expect_warning(
     ewspec.diff(x),
     "Data length is not power of two. Boundary correction has been applied."
+  )
+})
+
+test_that("ewspec.diff warns when diff.number not recognised", {
+  skip_on_cran()
+  x <- stats::rnorm(256)
+  expect_warning(
+    ewspec.diff(x, diff.number = 3),
+    "Function only implements 1st or 2nd differences. Using 1st difference."
+  )
+})
+
+test_that("ewspec.diff warns when diff.number = 2 and lag != 1", {
+  skip_on_cran()
+  x <- stats::rnorm(256)
+  expect_warning(
+    ewspec.diff(x, diff.number = 2, lag = 2),
+    "When diff.number = 1, only lag = 1 is supported. Resetting lag to be 1."
   )
 })
 
