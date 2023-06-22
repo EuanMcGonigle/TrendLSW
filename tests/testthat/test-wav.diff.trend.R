950cb8fff4bf896c526de8a3ffe81ddf09179af0
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2023-06-22 09:34:32 +0100

    Update test-wav.diff.trend.R

--- tests/testthat/test-wav.diff.trend.R
+++ tests/testthat/test-wav.diff.trend.R
@@ -47,3 +47,21 @@ test_that("wav.diff.trend.est executes with normal = FALSE and boundary handling
   x.t <- wav.diff.trend.est(x, x.s, normal = FALSE, boundary.handle = TRUE)
   expect_equal(class(x.t), "numeric")
 })
+
+test_that("wav.diff.trend.est executes with calc.confint = TRUE", {
+  skip_on_cran()
+  x <- stats::rnorm(256) + seq(from = 0, to = 4, length = 256)
+  x.s <- ewspec.diff(x)
+  expect_equal(
+    class(wav.diff.trend.est(x, x.s, calc.confint = TRUE)),"list")
+})
+
+test_that("wav.diff.trend.est executes with non-dyadic data and calc.confint = TRUE", {
+  skip_on_cran()
+  x <- stats::rnorm(230) + seq(from = 0, to = 4, length = 230)
+  x.s <- suppressWarnings(ewspec.diff(x))
+  expect_warning(
+    wav.diff.trend.est(x, x.s, calc.confint = TRUE),
+    "Data length is not power of two. Boundary correction has been applied."
+  )
+})
