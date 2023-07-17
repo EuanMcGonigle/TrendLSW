46d4aaa46be80aee76d69df1f10e62f87f50d5c8
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2023-07-17 14:56:16 +0100

    Update summary.TLSW.R

--- R/summary.TLSW.R
+++ R/summary.TLSW.R
@@ -9,52 +9,49 @@
 #'
 #' @examples
 summary.TLSW <- function(object, ...) {
-
-  if(object$do.spec.est == TRUE){
+  if (object$do.spec.est == TRUE) {
     cat("Spectral estimation was performed:\n")
-    if(object$spec$WP.smooth == TRUE){
+    if (object$spec$WP.smooth == TRUE) {
       cat("-smoothing was performed using ", object$spec$smooth.type, " smoothing with binwidth ", object$spec$binwidth, ".\n", sep = "")
-    } else{
+    } else {
       cat("-no smoothing was performed.\n")
     }
-    if(!is.null(object$spec$lag)){
-      if(object$spec$diff.number == 2){
+    if (!is.null(object$spec$lag)) {
+      if (object$spec$diff.number == 2) {
         cat("-time series was second differenced before wavelet transform applied.\n")
-      } else{
-        cat("-time series was first differenced at lag ", object$spec$lag," before wavelet transform applied.\n", sep = "")
+      } else {
+        cat("-time series was first differenced at lag ", object$spec$lag, " before wavelet transform applied.\n", sep = "")
       }
-
     }
     cat("-maximum wavelet scale analysed is scale ", object$spec$max.scale, ".\n", sep = "")
-    if(object$spec$boundary.handle == TRUE){
+    if (object$spec$boundary.handle == TRUE) {
       cat("-boundary handling was used.\n")
-    } else{
+    } else {
       cat("-no boundary handling was perfomed.\n")
     }
-
   } else {
     cat("Spectral estimation was not performed.\n")
   }
-  cat("----------------\n")
+  cat("-----------\n")
 
-  if(object$do.trend.est == TRUE){
+  if (object$do.trend.est == TRUE) {
     cat("Trend estimation was performed:\n")
-    if(object$trend$T.est.type == "linear"){
-      cat("-estimation was performed using a ", object$trend$T.transform, "imated wavelet transform with ", object$trend$T.est.type," thresholding.\n", sep = "")
+    if (object$trend$T.est.type == "linear") {
+      cat("-estimation was performed using a ", object$trend$transform.type, "imated wavelet transform with ", object$trend$T.est.type, " thresholding.\n", sep = "")
     } else {
-      cat("-estimation was performed using a ", object$trend$T.transform, "imated wavelet transform with ", object$trend$T.est.type," thresholding using a ", object$trend$T.thresh.type, " threshold.\n", sep = "")
+      cat("-estimation was performed using a ", object$trend$transform.type, "imated wavelet transform with ", object$trend$T.est.type, " thresholding using a ", object$trend$thresh.type, " threshold.\n", sep = "")
     }
 
     cat("-maximum wavelet scale analysed is scale ", object$trend$max.scale, ".\n", sep = "")
-    if(object$trend$boundary.handle == TRUE){
+    if (object$trend$boundary.handle == TRUE) {
       cat("-boundary handling was used.\n")
-    } else{
+    } else {
       cat("-no boundary handling was perfomed.\n")
     }
-
+    if (object$trend$calc.confint == TRUE) {
+      cat("-a pointwise " , 100*(1-object$trend$sig.lvl), "% confidence interval was calculated.", sep = "")
+    }
   } else {
     cat("Trend estimation was not performed.\n")
   }
-
-
 }
