2cdce4443a1040155328dddf8862e511d4703b65
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2023-07-21 11:56:11 +0100

    Update test-wav.trend.R

--- tests/testthat/test-wav.trend.R
+++ tests/testthat/test-wav.trend.R
@@ -62,9 +62,47 @@ test_that("calc.confint can't be used with nondec transform", {
   )
 })
 
+test_that("sig.lvl recognised", {
+  expect_error(
+    wav.trend.est(stats::rnorm(64), sig.lvl = 2),
+    "Error: sig.lvl must be a number between 0 and 1."
+  )
+})
+
 test_that("transform.type recognised", {
   expect_error(
     wav.trend.est(stats::rnorm(64), transform.type = "packet"),
     "Parameter transform.type must be either 'dec' or 'nondec'"
+  )
+})
+
+test_that("max.scale is integer", {
+  expect_error(
+    wav.trend.est(stats::rnorm(64), max.scale = 3.7),
+    "max.scale parameter must be an integer."
+  )
+})
+
+test_that("max.scale isn't too large", {
+  expect_warning(
+    wav.trend.est(stats::rnorm(64), max.scale = 9),
+    "max.scale parameter is outside valid range. Resetting to default value."
+  )
+})
+
+test_that("error for NA data", {
+  x <- rep(NA, 64)
+  expect_error(
+    wav.trend.est(x),
+    "Data contains mising values."
+  )
+})
+
+
+test_that("data is numeric", {
+  x <- c("1", "2")
+  expect_error(
+    wav.trend.est(x),
+    "Data is not numeric"
   )
 })
