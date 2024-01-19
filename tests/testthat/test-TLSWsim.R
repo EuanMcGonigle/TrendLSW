238bb9f0c36a78e9832d29e57d275574e2c6e21f
EuanMcGonigle <51928132+EuanMcGonigle@users.noreply.github.com>
2024-01-19 11:22:21 +0000

    Create test-TLSWsim.R

---
+++ tests/testthat/test-TLSWsim.R
@@ -0,0 +1,70 @@
+spec1 <- wavethresh::cns(256)
+spec1 <- wavethresh::putD(spec1, level = 6, rep(1, 256))
+
+spec2 <- matrix(0, 8, 2^8)
+spec2[1, ] <- rep(1, 256)
+
+spec3 <- vector(mode = "list", length = 8)
+spec3[[1]] <- function(u){1}
+
+test_that("TLSWsim executes with wd spec", {
+  x <- TLSWsim(spec = spec1)
+  expect_equal(class(x), "numeric")
+})
+
+test_that("TLSWsim executes with matrix spec", {
+  x <- TLSWsim(spec = spec2)
+  expect_equal(class(x), "numeric")
+})
+
+test_that("TLSWsim executes with list spec", {
+  x <- TLSWsim(spec = spec3)
+  expect_equal(class(x), "numeric")
+})
+
+test_that("TLSWsim executes with numeric trend", {
+  x <- TLSWsim(trend = rep(0, 256), spec = spec1)
+  expect_equal(class(x), "numeric")
+})
+
+test_that("TLSWsim executes with function trend", {
+  x <- TLSWsim(trend = function(u){0}, spec = spec1)
+  expect_equal(class(x), "numeric")
+})
+
+test_that("spectrum is positive", {
+  spec1$D[1] <- -10
+  expect_error(
+    TLSWsim(spec = spec1),
+    "All spectral elements must be non-negative."
+  )
+})
+
+test_that("innov.func argument is a function", {
+  expect_error(
+    TLSWsim(spec = spec1, innov.func = 1),
+    "Argument'innov.func' should be a function."
+  )
+})
+
+test_that("innov.func argument is an rnorm type function", {
+  expect_error(
+    TLSWsim(spec = spec1, innov.func = function(u){1}),
+    "Invalid 'innov.func' argument: should be in the rnorm family of functions."
+  )
+})
+
+
+test_that("spec matrix dimensions match", {
+  expect_error(
+    TLSWsim(spec = spec2[1:4,]),
+    "Dimensions of spec matrix incorrect. Number of columns should be 2 to the power of the number of rows."
+  )
+})
+
+test_that("trend and spec dimensions match", {
+  expect_error(
+    TLSWsim(trend = rep(0,10), spec = spec1),
+    "Length of trend does not match dimensions of spec."
+  )
+})
