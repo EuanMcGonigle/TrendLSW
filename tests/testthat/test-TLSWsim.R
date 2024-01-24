3fdefa4e2a1aa056bd00d4245ce854d36272ac92
Rebecca Killick <r.killick@lancaster.ac.uk>
2024-01-24 22:32:11 +0000

    Update test-TLSWsim.R
    Adding tests

--- tests/testthat/test-TLSWsim.R
+++ tests/testthat/test-TLSWsim.R
@@ -68,3 +68,13 @@ test_that("trend and spec dimensions match", {
     "Length of trend does not match dimensions of spec."
   )
 })
+
+test_that("TLSWsim gives same output",{
+  skip_on_cran()
+  spec <- matrix(0, nrow = 9, ncol = 512)
+  spec[1,] <- 1 + sin(seq(from = 0, to = 2 * pi, length = 512))^2
+  trend <- seq(from = 0, to = 5, length = 512)
+  set.seed(1)
+  x <- TLSWsim(trend = trend, spec = spec)
+  expect_snapshot_value(x)
+}
