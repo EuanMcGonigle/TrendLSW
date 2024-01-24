07cf94fc12f75f661e84c52d0c1b52ff8aa8ce3d
Rebecca Killick <r.killick@lancaster.ac.uk>
2024-01-24 22:51:23 +0000

    Update test-TLSW-utils.R
    Increasing code coverage

--- tests/testthat/test-TLSW-utils.R
+++ tests/testthat/test-TLSW-utils.R
@@ -17,7 +17,7 @@ test_that("print TLSW gives same output",{
   set.seed(1)
   x <- TLSWsim(trend = trend, spec = spec)
   x.TLSW <- TLSW(x)
-  expect_snapshot_value(print(x.TLSW))
+  expect_snapshot(print(x.TLSW))
 })
 
 test_that("plot TLSW gives same output",{
@@ -29,4 +29,14 @@ test_that("plot TLSW gives same output",{
   x <- TLSWsim(trend = trend, spec = spec)
   x.TLSW <- TLSW(x)
   expect_snapshot_file(pdf(file='plotTLSW.pdf');plot(x.TLSW);dev.off)
+})
+
+test_that("TLSW.lacf.calc runs on defaults",{
+  skip_on_cran()
+  spec <- matrix(0, nrow = 9, ncol = 512)
+  spec[1,] <- 1 + sin(seq(from = 0, to = 2 * pi, length = 512))^2
+  trend <- seq(from = 0, to = 5, length = 512)
+  set.seed(1)
+  x <- TLSWsim(trend = trend, spec = spec)
+  expect_snapshot_value(TLSW.lacf.calc(x))
 })
